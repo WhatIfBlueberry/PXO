131266b7bc06ab78690a65c1b7cc3d9c20c15552
Dylan <Dylan-Pascal@web.de>
2022-09-19 16:35:23 +0200

    Update Bishop.java

--- queenProblemSolver/src/chessPieces/Bishop.java
+++ queenProblemSolver/src/chessPieces/Bishop.java
@@ -6,6 +6,14 @@ import java.util.ArrayList;
 public class Bishop implements SolveablePiece {
     @Override
     public ArrayList<Point> solve(Integer boardSize) {
-        return null;
+        ArrayList<Point> ret = new ArrayList<>();
+        for (int x = 0; x < boardSize; x++) {
+            for (int y = 0; y < boardSize; y++) {
+                if ((x == 0 || x == boardSize - 1) && y != 0) {
+                    ret.add(new Point(x, y));
+                }
+            }
+        }
+        return ret;
     }
 }
