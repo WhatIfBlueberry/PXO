081638e3d3a5311944066262db19df1ca2028c62
Dylan <Dylan-Pascal@web.de>
2022-09-21 09:05:26 +0200

    smol changes to king.java, own tile got neglected

--- queenProblemSolver/src/solveableChessPieces/King.java
+++ queenProblemSolver/src/solveableChessPieces/King.java
@@ -7,7 +7,7 @@ public class King extends GenericChessPiece {
         for (int x = -1; x <= 1; x++) {
             if (!(row - x < 0 || row - x >= board.length)) {
                 for (int y = -1; y <= 1; y++) {
-                    if (!(col - y < 0 || col - y >= board.length) && (x != 0 && y != 0)) {
+                    if (!(col - y < 0 || col - y >= board.length)) {
                         if (board[row - x][col - y] == 1) {
                             return false;
                         }
