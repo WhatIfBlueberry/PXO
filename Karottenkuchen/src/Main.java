a0c5b7b3987f37dc608b26134b568259bdee01a7
Dylan <Dylan-Pascal@web.de>
2022-09-23 09:08:36 +0200

    deleted TestHelper

--- Karottenkuchen/src/Main.java
+++ Karottenkuchen/src/Main.java
@@ -1,11 +1,11 @@
 import com.wib.cyberwirtzlib.cyberwirtzimpl.CyberWirtz;
 import com.wib.cyberwirtzlib.cyberwirtzimpl.CyberWirtzFactory;
+import com.wib.cyberwirtzlib.cyberwirtzimpl.RealCyberWirtz;
 import com.wib.cyberwirtzlib.math.ComplexNumber;
 import com.wib.cyberwirtzlib.math.Fields;
+import com.wib.cyberwirtzlib.math.matrix.Matrix;
 
 public class Main {
     public static void main(String[] args) {
-        TestHelper helper = new TestHelper();
-        helper.multiplyComplex();
     }
 }
